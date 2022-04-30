39f8efc412ce353f22a7241825ec715740fb8e18
Jent0s <brevnov.eugeni@yandex.ru>
2022-04-30 15:24:23 +0300

    change taboles

--- migrations/0004_create-collections-table.sql
+++ migrations/0004_create-collections-table.sql
@@ -3,6 +3,7 @@ CREATE TABLE "collections" (
     "walletId" varchar NOT NULL,
     name varchar,
     description text,
+    picture varchar,
     "createdAt" timestamp(0) DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY ("walletId") REFERENCES "users" ("walletId") ON DELETE CASCADE
 );
